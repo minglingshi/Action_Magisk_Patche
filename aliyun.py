1865fea963ecbd25911d3aa524366557a44a4c71
sun <552143125@qq.com>
2023-06-30 16:23:48 +0800

    remove aliyun.py

--- aliyun.py
+++
@@ -1,7 +0,0 @@
-from aligo import Aligo
-from aligo import Auth
-
-
-ali = Aligo(name='refresh_token' , refresh_token='c5fd8e0644d64516983579fefad10e0d')
-
-ali.upload_folder('/home/runner/Magisk/done', parent_file_id='6446693a452a7031169f475ea109ca159c1c01bc')   
